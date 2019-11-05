c7b489c44a4ed85e333c5ac0435ec75061353b19
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-05 16:28:20 +0900

    データのJSON化に対応
    - 各データクラスに自身を辞書に変換するメソッドを追加
    - デフォルトのデータを作成する関数を追加
    - TODO: いちいち手動で辞書にしなくても自動でやる方法があるらしいので後で調べて変更する

--- sekigae.py
+++ sekigae.py
@@ -1,6 +1,8 @@
 """
 席替えアプリの実装
 """
+import json
+
 
 # 個人のデータを扱うクラス
 class PersonalData:
@@ -12,25 +14,129 @@ class PersonalData:
         self.buddy_history_list = []    # 過去隣に誰が座っていたかの履歴
         self.seat_history_list = []     # 過去に座ったシートの履歴
 
+    def load_from_dict(self):
+        pass
+
+    def convert_to_dict(self):
+        dic = dict()
+        dic['id'] = self.id
+        dic['name'] = self.name
+        dic['is_lefty'] = self.is_lefty
+        dic['forward_prio'] = self.forward_prio
+        dic['buddy_history_list'] = self.buddy_history_list
+        dic['seat_history_list'] = self.seat_history_list
+        return dic
+
 
 # 席のデータを扱うクラス
 class SeatData:
     def __init__(self):
-        self.global_index = 0   # 席全体の番号
-        self.local_index = 0    # テーブル内での番号
+        self.index = 0   # 席の番号
         self.table_number = 0   # テーブル番号
-        self.lefty_flag = False # 左利きが座れるかどうか
-        self.forward_flag = False   # 前方優先の席かどうか
+        self.lefty_flag = False     # 左利きが座れるかどうか
+        self.forward_flag = False   # 前方の席かどうか
+
+    def load_from_dict(self):
+        pass
+
+    def convert_to_dict(self):
+        dic = dict()
+        dic['index'] = self.index
+        dic['table_number'] = self.table_number
+        dic['lefty_flag'] = self.lefty_flag
+        dic['forward_flag'] = self.forward_flag
+        return dic
 
 
 # 席替えのデータを扱うクラス
 class SekigaeData:
-    def __init__(self):
-        self.member_list = []   # PersonalDataを格納
-        self.seat_list = []     # SeatDataを格納
-        self.order_list = []    # 個人IDを席順に格納する配列
+    def __init__(self, file_path=None):
+        self.member_list = None  # PersonalDataを格納
+        self.seat_list = None    # SeatDataを格納
+        self.order_dict = None   # 個人IDを席順に格納する配列
+
+        if not file_path:
+            self.member_list = []
+            self.seat_list = []
+            self.order_dict = dict()
+        else:
+            self.load_from_file(file_path)
+
+    def load_from_dict(self, dic):
+        self.member_list = []
+        self.seat_list = []
+        self.order_dict = dict()
+
+    def load_from_file(self, file_path):
+        file = open(file_path, 'r')
+        json.load(file)
+        file.close()
+        self.load_from_dict(json)
+
+    def convert_to_dict(self):
+        dic = dict()
+
+        member_list = []
+        for member in self.member_list:
+            member_list.append(member.convert_to_dict())
+        dic['member_list'] = member_list
+
+        seat_list = []
+        for seat in self.seat_list:
+            seat_list.append(seat.convert_to_dict())
+        dic['seat_list'] = seat_list
+
+        dic['order_dict'] = self.order_dict
+
+        return dic
 
 
 # filename...前回の席替え結果ファイル
 def run(filename):
     pass
+
+
+# デフォルトの席替えデータを作成する
+def create_default_sekigae_data():
+    data = SekigaeData()
+
+    # メンバーリスト
+    for i in range(0, 14):
+        personal_data = PersonalData()
+        personal_data.id = i
+        personal_data.name = f'member{i:02}'
+        personal_data.is_lefty = False
+        personal_data.forward_prio = 0
+        data.member_list.append(personal_data)
+
+    # 席データ
+    for i in range(0, 14):
+        seat_data = SeatData()
+        seat_data.index = i
+        if i == 0 or i == 3 or i == 4 or i == 7 or i == 8 or i == 13:
+            seat_data.lefty_flag = True
+        else:
+            seat_data.lefty_flag = False
+
+        if i <= 3:
+            seat_data.table_number = 0
+            seat_data.forward_flag = True
+        elif 3 < i <= 7:
+            seat_data.table_number = 1
+            seat_data.forward_flag = True
+        else:
+            seat_data.table_number = 2
+            seat_data.forward_flag = False
+        data.seat_list.append(seat_data)
+
+    # 席順データ
+    for member in data.member_list:
+        data.order_dict[member.id] = member.id
+
+    return data
+
+
+data = create_default_sekigae_data()
+data_dic = data.convert_to_dict()
+json_str = json.dumps(data_dic, ensure_ascii=False, indent=2, separators=(',', ': '))
+print(json_str)
