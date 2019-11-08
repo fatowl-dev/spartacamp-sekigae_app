e43450983d676dabbf43f3890da15bbdcc53cfc6
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-08 16:01:18 +0900

    GUI部分とりあえず完成
    - フォントファイルの追加

--- sekigae_gui.py
+++ sekigae_gui.py
@@ -1,31 +1,62 @@
+import datetime
+import json
+from glob import glob
+
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.factory import Factory
-from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
-from kivy.properties import ListProperty
-from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
-import os
+from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.properties import ListProperty
+from kivy.properties import StringProperty
+from kivy.resources import resource_add_path
+from kivy.uix.floatlayout import FloatLayout
 
-Config.set('graphics', 'width', '640')
-Config.set('graphics', 'height', '640')
+import sekigae
 
 
+# メインの処理をするクラス(ウィジェット)
 class SekigaeWidget(FloatLayout):
-    file_path = StringProperty('File:')
-    names = ListProperty(['mem' + str(f'{i:02d}') for i in range(14)])
+    file_path = StringProperty('None')
+    names = ListProperty(['mem' + str(f'{i:02d}') for i in range(14)])  # ここを書き換えると席の名前が変わる
+    file_list = ListProperty()
 
     def __init__(self, **kwargs):
         super(SekigaeWidget, self).__init__(**kwargs)
+        self.update_file_list()
 
-    def select_btn_clicked(self):
-        self.file_path = 'File: AAAAAAAAAAAAA'
+    def update_seat_order(self, data: sekigae.SekigaeData):
+        for i in range(len(self.names)):
+            key = str(i)
+            if key in data.order_dict:
+                self.names[i] = data.get_member_by_id(data.order_dict[str(i)]).name
+            else:
+                self.names[i] = 'NO DATA'
+
+    def update_file_list(self):
+        self.file_list = glob('json//*.json')
+
+    # ドロップダウンリストからファイルを選択したときの処理
+    def on_select_file(self, text):
+        self.file_path = text
+        data = sekigae.SekigaeData(self.file_path)
+        self.update_seat_order(data)
+
+    # シャッフルボタンを押したときの処理
+    def on_shuffle_btn_clicked(self):
+        data = sekigae.SekigaeData(self.file_path)
+        new_data = sekigae.execute(data)
+        self.update_seat_order(new_data)
+        # ファイルに保存
+        new_data_dict = new_data.convert_to_dict()
+        new_file_path = datetime.datetime.now().strftime('json/' + new_data.class_name + '_%Y%m%d%H%M%S.json')
+        file = open(new_file_path, 'w')
+        json.dump(new_data_dict, fp=file, ensure_ascii=False, indent=2, separators=(',', ': '))
+        file.close()
+        # ファイルリスト更新
+        self.file_path = new_file_path
+        self.update_file_list()
 
 
+# アプリケーションクラス
 class SekigaeApp(App):
     def __init__(self, **kwargs):
         super(SekigaeApp, self).__init__(**kwargs)
@@ -36,5 +67,11 @@ class SekigaeApp(App):
 
 
 if __name__ == '__main__':
+    # デフォルトに使用するフォントを変更する
+    resource_add_path('./fonts')
+    LabelBase.register(DEFAULT_FONT, 'ume-ugo5.ttf')  # 日本語が使用できるように日本語フォントを指定する
+
+    Config.set('graphics', 'width', '800')
+    Config.set('graphics', 'height', '800')
     app = SekigaeApp()
     app.run()
