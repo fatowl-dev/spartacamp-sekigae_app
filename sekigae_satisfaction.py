302ab742c38f4cdb1c4237d39e35f42b8d8b7f56
fatowl-dev <kingyo.summer.japan@gmail.com>
2019-11-10 09:14:44 +0900

    Add; shuffle_by_satisfactionを用意

--- sekigae_satisfaction.py
+++ sekigae_satisfaction.py
@@ -1,1 +1,9 @@
 # シャッフルのロジックに満足度を採用したもの
+# 特定回数完全ランダムのシャッフルを繰り返し、
+# 各シャッフルの満足度を評価、満足度が1番高い
+# 席順を採用する
+from sekigae import SekigaeData
+
+
+def shuffle_by_satisfaction(data: SekigaeData) -> SekigaeData:
+    pass
